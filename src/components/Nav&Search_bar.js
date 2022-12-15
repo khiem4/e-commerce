ab1b66f138ccc0f45202ac0ba04a97985e1caf97
khiem <khiemcan@gmail.com>
2022-12-16 00:38:23 +0700

    fixed & added nav dropdown

--- src/components/Nav&Search_bar.js
+++ src/components/Nav&Search_bar.js
@@ -1,5 +1,7 @@
 import { useState } from 'react'
-import { FaListUl, FaSearch } from 'react-icons/fa'
+import { FaListUl, FaSearch, FaMobileAlt, FaLaptop } from 'react-icons/fa'
+import { GiPaintedPottery, GiWatch } from 'react-icons/gi'
+import { MdOutlineChair } from 'react-icons/md'
 import './Nav&Search_bar.css'
 
 const NavSearchBar = () => {
@@ -18,8 +20,27 @@ const NavSearchBar = () => {
   return (
     <div className='nav-search'>
       <div className="nav">
-        <FaListUl size={30} />
-        <p>Category</p>
+        <FaListUl size={25} />
+        <span>Category</span>
+        <div className='nav-list'>
+          <div>
+            <FaMobileAlt />
+            <a href="#">Smart Phones</a>
+          </div>
+          <div>
+            <GiPaintedPottery />
+            <a href="#">Home Decoration</a>
+          </div>
+          <div>
+            <MdOutlineChair />
+            <a href="#">Furniture</a></div>
+          <div>
+            <FaLaptop />
+            <a href="#">Laptops</a></div>
+          <div>
+            <GiWatch />
+            <a href="#">Watches</a></div>
+        </div>
       </div>
       <form className='search'>
         <input type="text" name='search' placeholder='Search items...' />
