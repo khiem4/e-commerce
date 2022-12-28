d38f0a1d99e6f3a9609877576eea5adaed57a29b
khiem <khiemcan@gmail.com>
2022-12-29 00:26:24 +0700

    changed logo & updated responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,22 @@
-import { FaRegUser, FaCartPlus } from 'react-icons/fa'
+import { FaRegUser, FaCartArrowDown } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div className='header'>
-      <div className='logo'>
-        <img
-          src={require('../images/shop-logo.jpg')} alt="store-logo" />
+      <div className='logo_header'>
+        <img src={require('../images/logo.jpg')} alt="store-logo" />
       </div>
       <div className="services">
         <p className='support'>Customer support <br />
-          <span className='phone_number'>0905.xxxx.xxx</span></p>
+          <span className='phone_number'>0905.xxxx.xxx</span>
+        </p>
         <p className='hotline'>Hotline <br />
-          <span className='phone_number'>0925.xxxx.xxx</span></p>
+          <span className='phone_number'>0925.xxxx.xxx</span>
+        </p>
         <p className='feedback'>Service feedback <br />
-          <span className='phone_number'>090.xxxx.xxxx</span></p>
+          <span className='phone_number'>090.xxxx.xxxx</span>
+        </p>
       </div>
 
       <div className="user-cart">
@@ -22,15 +25,15 @@ const Header = () => {
             <FaRegUser className='user-dropdown' size={25} />
           </div>
           <div className='user-dropdown-content'>
-            <a className='user' href="#">Login</a>
-            <a className='user' href="#">Logout</a>
+            <Link to='/login'>Login</Link>
+            <a className='user' href="#">Sign-in</a>
           </div>
         </div>
         <div className="icons">
-          <FaCartPlus className='cart' size={25} />
+          <FaCartArrowDown className='cart' size={25} />
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
