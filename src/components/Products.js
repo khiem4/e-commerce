e18a7c61ec64b5aae3b329b0d544e284b9b4a533
khiem <khiemcan@gmail.com>
2023-01-30 01:50:03 +0700

    fixed addToCart function

--- src/components/Products.js
+++ src/components/Products.js
@@ -6,6 +6,7 @@ import productService from '../services/products'
 
 const Products = ({ products }) => {
   const [cart, setCart] = useState([])
+  console.log('cart:', cart)
   window.scrollTo({ top: 0, behavior: "smooth" })
 
   useEffect(() => {
@@ -16,18 +17,23 @@ const Products = ({ products }) => {
 
   const addToCart = async (product) => {
     const item = {
-      product: product.title,
+      title: product.title,
       thumbnail: product.thumbnail,
       price: product.price,
       quantity: 1,
-      id: product.id
     }
 
-    const findProductInCart = cart.find(product => product.id === item.id)
-    const updateQuantity = { ...item, quantity: findProductInCart.quantity + 1 }
+    const findProductInCart = cart.find(product =>
+      product.title === item.title)
 
     if (findProductInCart) {
-      const response = await productService.update(item.id, updateQuantity)
+      const updateQuantity = {
+        ...item,
+        quantity: findProductInCart.quantity + 1,
+        id: findProductInCart.id
+      }
+
+      const response = await productService.update(updateQuantity)
       const cartUpdated = cart.map(product =>
         product.id !== response.id ? product : response)
 
@@ -52,7 +58,7 @@ const Products = ({ products }) => {
                 <BsFillCartPlusFill size={20} />
               </button>
             </div>
-            <img src={product.thumbnail} />
+            <img src={product.thumbnail} alt={product.title} />
             <div className='product_info'>
               <Link
                 to={`/products/${product.id}`}>{product.title}
