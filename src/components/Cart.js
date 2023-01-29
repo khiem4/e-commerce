f38ed8ae366e8abd24f4a34fb664fb74c3c656a4
khiem <khiemcan@gmail.com>
2023-01-30 01:48:58 +0700

    updated cart component and css

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,63 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import productService from '../services/products'
+
 const Cart = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    productService
+      .getCart()
+      .then(res => setProducts(res))
+  }, [])
+
+  const handleRemove = async (id) => {
+    await productService.remove(id)
+    const updatedProducts = products.filter(product => product.id !== id)
+    setProducts(updatedProducts)
+  }
+
+  const totalPrice = products.reduce((acc, obj) => acc + obj.price, 0)
+
   return (
-    <div className='cart_container'>
-      <h2>Shopping Cart</h2>
-      <div className='cart'>
-        <div className='cart_head'>
-          <div>Product</div>
-          <div>Quantity</div>
-          <div>Price</div>
+    <>
+      <h2 className='shopping_cart'>Shopping Cart</h2>
+      <div className='cart_container'>
+        <div className='table_checkout_products'>
+          <table>
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            {products.map(product =>
+              <tbody className='table_data_product'>
+                <tr>
+                  <td>
+                    <img src={product.thumbnail} alt="image" />{product.title}
+                  </td>
+                  <td>{product.quantity}</td>
+                  <td>${product.price}</td>
+                  <td>
+                    <button type='button' onClick={() => handleRemove(product.id)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            )}
+          </table>
         </div>
-        <div>
-          <h3>Total price</h3>
+        <div className='check_out'>
+          <p>Total:  ${totalPrice}</p>
           <button>Check Out</button>
+          <button>Continue Shopping</button>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
