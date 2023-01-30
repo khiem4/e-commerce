a401d27d5b61d35f594e2da8de53dfc5499520ba
khiem <khiemcan@gmail.com>
2023-01-31 00:30:53 +0700

    added useNavigate and css responsive

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import productService from '../services/products'
 
 const Cart = () => {
   const [products, setProducts] = useState([])
+  const navigate = useNavigate()
 
   useEffect(() => {
     productService
@@ -17,13 +19,17 @@ const Cart = () => {
     setProducts(updatedProducts)
   }
 
+  const handleNavigate = () => {
+    navigate('/')
+  }
+
   const totalPrice = products.reduce((acc, obj) => acc + obj.price, 0)
 
   return (
     <>
       <h2 className='shopping_cart'>Shopping Cart</h2>
       <div className='cart_container'>
-        <div className='table_checkout_products'>
+        <div className='table_data_product'>
           <table>
             <thead>
               <tr>
@@ -34,12 +40,13 @@ const Cart = () => {
               </tr>
             </thead>
             {products.map(product =>
-              <tbody className='table_data_product'>
+              <tbody>
                 <tr>
                   <td>
-                    <img src={product.thumbnail} alt="image" />{product.title}
+                    <img src={product.thumbnail} alt={product.title} />
+                    {product.title}
                   </td>
-                  <td>{product.quantity}</td>
+                  <td>x {product.quantity}</td>
                   <td>${product.price}</td>
                   <td>
                     <button type='button' onClick={() => handleRemove(product.id)}>
@@ -52,9 +59,13 @@ const Cart = () => {
           </table>
         </div>
         <div className='check_out'>
-          <p>Total:  ${totalPrice}</p>
+          <p>
+            Total: <span>${totalPrice}</span>
+          </p>
           <button>Check Out</button>
-          <button>Continue Shopping</button>
+          <button onClick={handleNavigate}
+          >Continue Shopping
+          </button>
         </div>
       </div>
     </>
