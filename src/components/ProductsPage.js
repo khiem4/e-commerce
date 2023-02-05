53e700f0c652090b9d5808a4c9b04793d9daa605
khiem <khiemcan@gmail.com>
2023-02-05 23:48:17 +0700

    Changed name

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react'
+import Categories from './Categories'
 import Pagination from './Pagination'
 import Products from './Products'
 import SearchBar from './SearchBar'
+import SortPrice from './SortPrice'
 
-const ProductsList = ({ products }) => {
+const ProductsPage = ({ products, productCategories }) => {
   const [productsPriceSort, setProductsPriceSort] = useState([])
   const [toggle, setToggle] = useState('')
   const [currentPage, setCurrentPage] = useState(1)
@@ -20,49 +22,47 @@ const ProductsList = ({ products }) => {
   const currentProducts = productsPriceSort.slice(indexOfFirstProduct, indexOfLastProduct)
   const paginate = pageNumber => setCurrentPage(pageNumber)
 
-  const handleFilter = (value) => {
-    setFilter(value)
-  }
-
   const findProduct = filter
     ? productsPriceSort.filter(product =>
       product.title.toLowerCase().includes(filter.toLowerCase()))
     : currentProducts
 
-  const filterLowToHigh = () => {
+  const sortLowToHigh = () => {
     const sort = [...products].sort((a, b) => a.price - b.price)
     setProductsPriceSort(sort)
     setToggle('low_to_high')
   }
 
-  const filterHighToLow = () => {
+  const sortHighToLow = () => {
     const sort = [...products].sort((a, b) => b.price - a.price)
     setProductsPriceSort(sort)
     setToggle('high_to_low')
   }
 
+  const handleFilter = (word) => {
+    setFilter(word)
+  }
+
   return (
     <>
       <SearchBar handleFilter={handleFilter} />
-      <div className='products_price_sort'>
-        <button
-          className={toggle === 'high_to_low' ? 'high_to_low' : ''}
-          onClick={filterHighToLow}>Price: high to low
-        </button>
-        <button
-          className={toggle === 'low_to_high' ? 'low_to_high' : ''}
-          onClick={filterLowToHigh}>Price: low to high
-        </button>
-      </div>
+      <Categories productCategories={productCategories} />
+      <SortPrice
+        sortLowToHigh={sortLowToHigh}
+        sortHighToLow={sortHighToLow}
+        toggle={toggle}
+      />
       <Products products={findProduct} />
-      {findProduct.length > 5 &&
+      {
+        findProduct.length > 5 &&
         <Pagination
           productsPerPage={productsPerPage}
           totalProducts={productsPriceSort.length}
           paginate={paginate}
-        />}
+        />
+      }
     </>
   )
 }
 
-export default ProductsList
+export default ProductsPage
