eb0637215c91b4a4cfdc3f2b13d465b894d6c72c
khiem <khiemcan@gmail.com>
2023-02-07 02:00:21 +0700

    fixed productCategory

--- src/App.js
+++ src/App.js
@@ -1,56 +1,59 @@
 import { Route, Routes, useMatch } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import Header from './components/Header'
-import NavSearchBar from './components/Nav&Search_bar'
 import Banner from './components/Banner'
 import LoginForm from './components/LoginForm'
 import RegisterForm from './components/RegisterForm'
 import Footer from './components/Footer'
-import Products from './components/Products'
 import Product from './components/Product'
 import Cart from './components/Cart'
 import productService from './services/products'
-import ProductsList from './components/ProductsList'
+import ProductsPage from './components/ProductsPage'
+import ScrollToTop from './components/ScrollToTop'
 
 const App = () => {
   const [products, setProducts] = useState([])
   const match = useMatch('/products/:id')
 
   useEffect(() => {
-    productService
-      .getAll()
-      .then(products => setProducts(products))
+    async function fetchProducts() {
+      const response = await productService.getAll()
+      setProducts(response)
+    }
+    fetchProducts()
   }, [])
 
   const product = match
     ? products.find(item => item.id === Number(match.params.id))
     : null
 
-  const categoriesFilter = [...new Set(products.map(product => product.category))]
+  const productCategories = [...new Set(products.map(product => product.category))]
 
   return (
     <>
+      <ScrollToTop />
       <Header />
-      <NavSearchBar products={products} categories={categoriesFilter} />
       <Routes>
-        <Route path='/' element={
-          <>
-            <Banner />
-            <div className='multiple_products_container'>
-            </div>
-          </>}
-        />
+        <Route path='/' element={<Banner />} />
         <Route path='/products'>
-          <Route path='search' />
-          <Route path=':id' element={<Product product={product} />} />
-          {
-            categoriesFilter.map((category, index) =>
-              <Route
-                key={index}
-                path={category}
-                element={<ProductsList products={products.filter(items =>
-                  items.category === category)} />}
-              />)
+          <Route
+            path='/products'
+            element={<ProductsPage products={products} categories={productCategories} />}
+          />
+          <Route
+            path=':id'
+            element={<Product product={product} />}
+          />
+          {productCategories.map((category, index) =>
+            <Route
+              key={index}
+              path={category}
+              element={
+                <ProductsPage
+                  products={products.filter(items =>
+                    items.category === category)}
+                  categories={productCategories} />}
+            />)
           }
         </Route>
         <Route path='/login' element={<LoginForm />} />
