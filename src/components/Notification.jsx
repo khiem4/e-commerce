18546170f4f69e54bba13136f2590c64c599aba2
khiem <khiemcan@gmail.com>
2023-02-27 20:17:04 +0700

    adjusted notification object

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,20 +1,24 @@
 import { useSelector } from 'react-redux';
-import { MdVerified } from 'react-icons/md'
+import { MdError, MdVerified } from 'react-icons/md'
 
 const Notification = () => {
   const notification = useSelector(state => state.notification)
 
   return (
-    <div className='notification_popup'>
-      {notification.map((notify, index) =>
-        <div key={index}>
-          <div>
-            <MdVerified />
-            {notify}
-          </div>
+    <>
+      {notification.success &&
+        <div className='notification_success'>
+          <MdVerified />
+          {notification.success}
         </div>
-      )}
-    </div>
+      }
+      {notification.error &&
+        <div className='notification_error'>
+          <MdError />
+          {notification.error}
+        </div>
+      }
+    </>
   )
 }
 
