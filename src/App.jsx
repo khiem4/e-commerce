de2a7ccf01c6d20e2eaa24ebe20d0f14837ae41e
khiem <khiemcan@gmail.com>
2023-03-01 02:04:49 +0700

    updated component and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes, useLocation, } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
 import Header from './components/Header'
@@ -10,14 +10,15 @@ import Footer from './components/Footer'
 import Product from './components/Product'
 import ProductsPage from './components/ProductsPage'
 import ScrollToTop from './components/ScrollToTop'
+import Breadcrumb from './components/Breadcrumb'
 import { getAllProducts } from './reducers/productsReducer'
 import { getAllProductsInCart } from './reducers/cartReducer'
 import { isUserLogged } from './reducers/userReducer'
 
 const App = () => {
   const dispatch = useDispatch()
-  const products = useSelector(state => state.products)
   const user = useSelector(state => state.user)
+  const location = useLocation().pathname
 
   useEffect(() => {
     dispatch(getAllProducts())
@@ -25,12 +26,13 @@ const App = () => {
     dispatch(isUserLogged())
   }, [dispatch])
 
-  const categories = [...new Set(products.map(product => product.category))]
-
   return (
     <>
       <ScrollToTop />
       <Header />
+      {location !== '/'
+        ? <Breadcrumb />
+        : null}
       <Routes>
         <Route
           path='/'
@@ -47,27 +49,23 @@ const App = () => {
           path='/register'
           element={<RegisterForm />}
         />
-        <Route
-          path='/cart'
-          element={<CartPage />}
-        />
-        <Route path='/products'>
+        <Route path='/products' >
           <Route
-            path='/products/all'
-            element={<ProductsPage />}
+            index element={<ProductsPage />}
           />
           <Route
             path=':id'
             element={<ProductsPage />}
           />
-          {categories.map((category, index) =>
-            <Route
-              key={index}
-              path={`${category}/:id`}
-              element={<Product />}
-            />
-          )}
+          <Route
+            path={`:id/:id`}
+            element={<Product />}
+          />
         </Route>
+        <Route
+          path='/cart'
+          element={<CartPage />}
+        />
       </Routes>
       <Footer />
     </>
