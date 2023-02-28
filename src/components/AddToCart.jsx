03589293af4467204054e6120a5e3010eef4aa92
khiem <khiemcan@gmail.com>
2023-03-01 01:59:59 +0700

    added condition check user

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -1,13 +1,18 @@
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { addProductToCart } from '../reducers/cartReducer'
-import { successMessage, removeMessage } from '../reducers/notificationReducer'
+import { successMessage, removeMessage, errorMessage } from '../reducers/notificationReducer'
 
 const AddToCart = ({ product, quantity, className, children }) => {
   const dispatch = useDispatch()
+  const user = useSelector(state => state.user)
 
   function addToCart() {
-    dispatch(addProductToCart(product, quantity))
+    if (!user) {
+      dispatch(errorMessage('Sorry you need to login first'))
+      return dispatch(removeMessage(5000))
+    }
 
+    dispatch(addProductToCart(product, quantity))
     dispatch(successMessage(`Product has been added to cart`))
     dispatch(removeMessage(2000))
   }
