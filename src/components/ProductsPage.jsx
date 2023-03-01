36addbfbea997496a92888ce0825f2353d0499de
khiem <khiemcan@gmail.com>
2023-03-02 01:14:25 +0700

    moved SearchBar , Categories to App

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -1,20 +1,11 @@
-import { useState } from 'react'
-import Categories from './Categories'
 import ProductsCard from './ProductsCard'
-import SearchBar from './SearchBar'
 import SortPrice from './SortPrice'
 
 const ProductsPage = () => {
-  const [filter, setFilter] = useState('')
-
   return (
     <>
-      <div className='categories_and_search_bar'>
-        <Categories />
-        <SearchBar setFilter={setFilter} />
-      </div>
       <SortPrice />
-      <ProductsCard filter={filter} />
+      <ProductsCard />
     </>
   )
 }
