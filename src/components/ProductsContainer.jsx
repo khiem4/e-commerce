ed384f1b815fb3b1d3810fe326a1c0ac949a8d87
khiem <khiemcan@gmail.com>
2023-03-02 21:53:46 +0700

    moved SortPrice component to ProductContainer

--- src/components/ProductsContainer.jsx
+++ src/components/ProductsContainer.jsx
@@ -1,39 +1,74 @@
+import { useEffect, useState } from 'react'
 import { BsFillCartPlusFill } from 'react-icons/bs'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import AddToCart from './AddToCart'
 
 const ProductsContainer = ({ products }) => {
+  const [toggle, setToggle] = useState('')
+  const [items, setItems] = useState([])
+  const location = useLocation().pathname
+
+  useEffect(() => {
+    setItems(products)
+  }, [products])
+
+  const sortLowToHigh = () => {
+    const sort = [...products].sort((a, b) => a.price - b.price)
+    setItems(sort)
+    setToggle('low_to_high')
+  }
+
+  const sortHighToLow = () => {
+    const sort = [...products].sort((a, b) => b.price - a.price)
+    setItems(sort)
+    setToggle('high_to_low')
+  }
+
   return (
-    <ul className='row'>
-      {products.map(product =>
-        <li
-          className='product_card'
-          key={product.id} >
-          <AddToCart
-            className={'btn_hover_cart'}
-            product={product}
-            quantity={1}>
-            <BsFillCartPlusFill size={20} />
-          </AddToCart>
-          <Link
-            to={`/products/${product.category}/${product.title}`}>
-            <img
-              src={product.thumbnail}
-              alt={product.title}
-            />
-            <div className='products_info'>
-              <p>{product.title}</p>
-              <div>
-                <span>${Math.round(
-                  product.price - (product.price / 100 * product.discountPercentage))}
-                </span>
-                <span>${product.price}</span>
+    <>
+      {location === '/'
+        ? null
+        : <div className='products_price_sort'>
+          <button
+            className={toggle === 'high_to_low' ? 'high_to_low' : ''}
+            onClick={sortHighToLow}>Price: high to low
+          </button>
+          <button
+            className={toggle === 'low_to_high' ? 'low_to_high' : ''}
+            onClick={sortLowToHigh}>Price: low to high
+          </button>
+        </div>}
+      <ul className='row'>
+        {items.map(product =>
+          <li
+            className='product_card'
+            key={product.id} >
+            <AddToCart
+              className={'btn_hover_cart'}
+              product={product}
+              quantity={1}>
+              <BsFillCartPlusFill size={20} />
+            </AddToCart>
+            <Link
+              to={`/products/${product.category}/${product.title}`}>
+              <img
+                src={product.thumbnail}
+                alt={product.title}
+              />
+              <div className='products_info'>
+                <p>{product.title}</p>
+                <div>
+                  <span>${Math.round(
+                    product.price - (product.price / 100 * product.discountPercentage))}
+                  </span>
+                  <span>${product.price}</span>
+                </div>
               </div>
-            </div>
-          </Link>
-        </li>
-      )}
-    </ul>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </>
   )
 }
 
