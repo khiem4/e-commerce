919cd4c477cd42887f024c5b467625bfd0c36910
khiem <khiemcan@gmail.com>
2023-03-03 01:43:53 +0700

    added handleSearch function

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,38 @@
 import { BsSearch } from 'react-icons/bs'
 import { IconContext } from "react-icons";
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setSearch } from '../reducers/searchReducer';
 
-const SearchBar = ({ setFilter }) => {
+const SearchBar = () => {
+  const [filter, setFilter] = useState('')
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    dispatch(setSearch(filter))
+    navigate(`/search`)
+  }
+
   return (
-    <IconContext.Provider value={{ className: "search_icon" }}>
-      <div className="search_bar">
-        <input
-          type="text"
-          name='search'
-          onChange={(e) => setFilter(e.target.value)}
-          placeholder='Search products...'
-        />
-        <BsSearch size={20} />
-      </div>
-    </IconContext.Provider>
+    <>
+      <IconContext.Provider value={{ className: "search_icon" }}>
+        <form className="search_bar">
+          <input
+            type="text"
+            name='search'
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+            placeholder='Search products...'
+          />
+          <button onClick={handleSearch}>
+            <BsSearch />
+          </button>
+        </form>
+      </IconContext.Provider>
+    </>
   )
 }
 
