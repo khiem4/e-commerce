7c858e29e3d2aaf9ccc53470bcc80165ada3a08d
khiem <khiemcan@gmail.com>
2023-03-03 23:18:25 +0700

    removed ProductsCard component

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -1,11 +1,70 @@
-import ProductsCard from './ProductsCard'
-import SortPrice from './SortPrice'
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useLocation, useParams } from 'react-router-dom'
+import { setSearch } from '../reducers/searchReducer'
+import Pagination from './Pagination'
+import ProductsContainer from './ProductsContainer'
 
 const ProductsPage = () => {
+  const [currentPage, setCurrentPage] = useState(1)
+  const [productsPerPage] = useState(15)
+  const dispatch = useDispatch()
+  const products = useSelector(state => state.products)
+  const search = useSelector(state => state.search)
+  const id = useParams().id
+  const location = useLocation().pathname
+
+  useEffect(() => {
+    setCurrentPage(1)
+
+    if (location !== '/search') {
+      dispatch(setSearch(null))
+    }
+  }, [])
+
+  const indexOfLastProduct = currentPage * productsPerPage
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage
+  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct)
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber)
+    window.scrollTo({ top: 100, behavior: 'smooth' })
+  }
+
+  if (id) {
+    const productsInCategory = products.filter(product =>
+      product.category === id)
+
+    return (
+      <ProductsContainer
+        products={productsInCategory}
+      />
+    )
+  }
+
+  if (search) {
+    const productsFiltered = products.filter(product =>
+      product.title.toLowerCase().includes(search.toLowerCase())
+      ||
+      product.category.toLowerCase().includes(search.toLowerCase())
+    )
+
+    return (
+      <ProductsContainer
+        products={productsFiltered}
+      />
+    )
+  }
+
   return (
     <>
-      <SortPrice />
-      <ProductsCard />
+      <ProductsContainer
+        products={currentProducts}
+      />
+      <Pagination
+        productsPerPage={productsPerPage}
+        totalProducts={products.length}
+        paginate={paginate}
+      />
     </>
   )
 }
