fda4a883a2c88ead8774c6416c0367b2e2a00005
khiem <khiemcan@gmail.com>
2023-03-06 01:11:39 +0700

    added navigate and fixed message

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -1,20 +1,21 @@
 import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import { addProductToCart } from '../reducers/cartReducer'
-import { successMessage, removeMessage, errorMessage } from '../reducers/notificationReducer'
+import { successMessage, errorMessage } from '../reducers/notificationReducer'
 
 const AddToCart = ({ product, quantity, className, children }) => {
   const dispatch = useDispatch()
   const user = useSelector(state => state.user)
+  const navigate = useNavigate()
 
   function addToCart() {
     if (!user) {
-      dispatch(errorMessage('Sorry you need to login first'))
-      return dispatch(removeMessage(5000))
+      navigate('/login')
+      return dispatch(errorMessage('Sorry you need to login first', 4000))
     }
 
     dispatch(addProductToCart(product, quantity))
-    dispatch(successMessage(`Product has been added to cart`))
-    dispatch(removeMessage(2000))
+    dispatch(successMessage(`Product has been added to cart`, 2000))
   }
 
   return (
